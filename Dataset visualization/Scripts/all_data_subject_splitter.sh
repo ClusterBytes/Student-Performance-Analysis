6f9077b883fcb47ff50de7df247b4641597a9710
Jarosh-Antony <jaroshantony64@gmail.com>
2023-02-21 20:14:20 +0530

    Add split data subject wise

--- Dataset visualization/Scripts/all_data_subject_splitter.sh
+++ Dataset visualization/Scripts/all_data_subject_splitter.sh
@@ -1,3 +1,12 @@
-for i in `cut -d "," -f 5 ./../../1.csv | sort | uniq | head -n -1`;do
-	echo $i ${i}
+dsdir='./../../Dataset'
+total=`wc -l ${dsdir}/CSV-Single/2_no_unwanted_grades.csv | cut -d ' ' -f 1` 
+k=0
+for i in `tail -n +2 ${dsdir}/CSV-Single/2_no_unwanted_grades.csv`;do
+	sub=`echo $i | cut -d ',' -f 5`
+	echo $i >> ${dsdir}/CSV-Subject/${sub}.csv
+	(( k++ ))
+	if(( k%1000==0 ));then
+		echo $k/$total
+	fi
 done
+
