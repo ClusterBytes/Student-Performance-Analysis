987fe1e8cd0e6f07090d1f7cf4e4b113c3e83cbc
Abhishek Ballullaya K <abhishekbk1806@gmail.com>
2023-02-27 22:09:25 +0530

    Updated onehotencoder code for our modified  dataset

--- Data preprocessing/onehotencoder.py
+++ Data preprocessing/onehotencoder.py
@@ -1,12 +1,20 @@
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
 
-data = pd.read_csv('D:/Main_Project/Student-Performance-Analysis/Datasets/1.csv')
-data1 = data.loc[(data['Dept']=='CSE') & (data['Semester']=='S1') & (data['Year']==2015)]
-print(data1)
-ct = ColumnTransformer([('others1','passthrough',slice(4)),
-                        ('onehot', OneHotEncoder(),['Subject']),
-                     ],remainder='passthrough')
-transformed=ct.fit_transform(data1)
-print(transformed[100:120])
+data = pd.read_csv('D:/Main_Project/Student-Performance-Analysis/Datasets/reordered.csv')
+
+year_dept = ['Year','Dept']
+subjects = ['S1','S2', 'S3', 'S4', 'S5', 'S6', 'S7']
+
+onehot_encode = Pipeline(steps = [('encoder', OneHotEncoder(sparse_output=False))])
+
+ct = ColumnTransformer([('noencoder', 'passthrough', year_dept),
+                        ('onehot', onehot_encode, subjects)],
+                        remainder='passthrough')
+transformed=ct.fit_transform(data)
+
+transformed_data = pd.DataFrame(transformed, columns = ct.get_feature_names_out())
+
+transformed_data.to_csv("result.csv")
