e8ce782135bbed03b68f53e3531e893b2c691ce4
Lince mathew <linz07m@gmail.com>
2023-02-28 14:44:11 +0530

    changing index

--- data-merging/data-merging.py
+++ data-merging/data-merging.py
@@ -8,8 +8,12 @@ merging_columns = ["Subject", "Attendance", "Internal mark", "Grade"]
 final = []
 for col in merging_columns:
     grouped = df.groupby("Reg_No")[col].apply(list)
-    grouped_df = pd.DataFrame(grouped.values.tolist(), index=grouped.index).add_prefix(col[0:3])
+    grouped_df = pd.DataFrame(grouped.values.tolist(), index=grouped.index)
+    columns = list(grouped_df.columns)
+    new_cols = [col[0:1] + str(i + 1) for i in range(len(columns))]
+    grouped_df.columns = new_cols
     final.append(grouped_df)
     print(grouped_df)
+
 df_final = ft.reduce(lambda left, right: pd.merge(left, right, on="Reg_No"), final)
 df_final.to_csv("final.csv", index=True)
