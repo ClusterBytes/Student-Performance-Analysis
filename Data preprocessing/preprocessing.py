dd218cb87a16c337c58c67934f46f9951fa9ecc9
Abhishek Ballullaya K <abhishekbk1806@gmail.com>
2023-02-28 15:40:48 +0530

    Done Ordinal encoding on grades and onehot encoding on dept

---
+++ Data preprocessing/preprocessing.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+
+data = pd.read_csv('D:/Main_Project/Student-Performance-Analysis/Datasets/reordered.csv')
+
+onehot_encode = OneHotEncoder(sparse_output=False)
+ordinal_encode_year = OrdinalEncoder()
+ordinal_encode_grade = OrdinalEncoder(categories=[['F', 'P', 'D', 'C', 'C+', 'B', 'B+', 'A', 'A+', 'S']] * 7)
+
+ct = ColumnTransformer([('Year', ordinal_encode_year, ['Year']),
+                        ('Dept', onehot_encode, ['Dept']),
+                        ('Sub', onehot_encode, slice(2,9)),
+                        ('Attendance', 'passthrough', slice(9,16)),
+                        ('Internal', 'passthrough', slice(16,23)),
+                        ('Grade', ordinal_encode_grade, slice(23,30))])
+
+transformed=ct.fit_transform(data)
+
+transformed_data = pd.DataFrame(transformed, columns = ct.get_feature_names_out())
+print(transformed_data)
+
+transformed_data.to_csv("result.csv")
