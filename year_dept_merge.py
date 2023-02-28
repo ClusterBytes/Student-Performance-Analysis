92edd826f98186d7199426bc18be25f650cfb529
jayakri10 <jayakrishnan.jayadevan@gmail.com>
2023-02-28 15:13:33 +0530

    wip removing duplicates

--- year_dept_merge.py
+++ year_dept_merge.py
@@ -1,12 +1,22 @@
 from pandas import *
 
-def year_dept_merge(data):    
-    # reading CSV file
-    # data = read_csv("1.csv")
-    df = DataFrame(data)
+# def year_dept_merge(data):    
+# reading CSV file
 
-    df = df.loc[:, ['Reg_No', 'Year', 'Dept']]
-    df = df.drop_duplicates()
-    # df.to_csv("Grouped_csv",index=False)
-    return df
+
+data = read_csv("1.csv")
+df = DataFrame(data)
+
+column_to_compare = 'Reg_No'
+
+df = df.loc[:, ['Reg_No', 'Year', 'Dept']]
+
+df = df.drop_duplicates()
+
+duplicates = df[df.duplicated(subset=column_to_compare, keep=False)]
+
+
+df.to_csv("Grouped.csv",index=False)
+# return df
+
 
