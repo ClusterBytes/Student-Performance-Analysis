c1ab2c30350babde66be2d6e72a2a411cfebb55b
jayakri10 <jayakrishnan.jayadevan@gmail.com>
2023-03-06 15:10:51 +0530

    Discarded year

--- data-merging/year_dept_merge.py
+++ data-merging/year_dept_merge.py
@@ -7,9 +7,12 @@ from pandas import *
 data = read_csv("1.csv")
 df = DataFrame(data)
 
+# df2 = DataFrame(data)
+
 column_to_compare = 'Reg_No'
 
-df = df.loc[:, ['Reg_No', 'Year', 'Dept']]
+
+df = df.loc[:, ['Reg_No','Dept',]]
 
 df = df.drop_duplicates()
 
@@ -17,6 +20,16 @@ duplicates = df[df.duplicated(subset=column_to_compare, keep=False)]
 
 
 df.to_csv("Grouped.csv",index=False)
+
+
+
+# # column_to_count = 'Year'
+# subjects_per_year = df2.groupby(['Reg_No', 'Year'])['Subject'].count()
+
+
+
+# print(subjects_per_year)
+
 # return df
 
 
