ac14e9f1e9ea62d7e04031b2a975210e4773f48c
Lince mathew <linz07m@gmail.com>
2023-03-07 16:44:54 +0530

    removing unwanted grades

---
+++ data-merging/remove_unwanted.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import functools as ft
+
+
+df = pd.read_csv("each_student_row.csv")
+# for i in range(1, 19):
+
+# print(df["G" + str(i)])
+# df_filtered = df[(df["G" + str(i)] != "Withheld")]
+
+# df.loc[(df["G" + str(i)] == "Withheld")]
+removing_items = ["Withheld", "Withheld*", "FE", "I", "Absent"]
+for item in removing_items:
+    df.replace(item, "F", inplace=True)
+
+print(df)
+
+df.to_csv("removed", index=False)
