1ef8ad42901ca35f602780ee50aef6a93df32fd5
Abhishek Ballullaya K <abhishekbk1806@gmail.com>
2023-04-16 17:49:48 +0530

    Applied OneVsRest classifer on LR

--- model_training/logistic_regression/logistic_regression.py
+++ model_training/logistic_regression/logistic_regression.py
@@ -2,8 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score
-import json
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+#import json
 from warnings import filterwarnings
 filterwarnings('ignore')
 
@@ -18,13 +19,13 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, random
 print("Splitting of x :\n", x_train.shape, x_test.shape)
 print("Splitting of y:\n",y_train.shape, y_test.shape)
 
-with open('logistic_regression_para.json', 'r') as fp:
-    js = json.load(fp)
+# with open('logistic_regression_para.json', 'r') as fp:
+#     js = json.load(fp)
 
-C = js['C_para']
-penalty = js['penalty_para']
+# C = js['C_para']
+# penalty = js['penalty_para']
  
-reg = []
+#reg = []
 grade = []
 accuracy = []
 total_acc = 0
@@ -33,18 +34,26 @@ for i in range(18):
     ind = 'G'+str(i+1)
     grade.append(ind)
     
-    log_reg = LogisticRegression(penalty=penalty[i], C=C[i])
+    log_reg = LogisticRegression()
 
-    log_reg.fit(x_train, y_train.loc[:,ind])
+    model = OneVsRestClassifier(log_reg)
 
-    y_pred = log_reg.predict(x_test)
+    model.fit(x_train, y_train.loc[:,ind])
 
-    reg.append(log_reg)
+    y_pred = model.predict(x_test)
+
+    #reg.append(log_reg)
     
     acc = accuracy_score(y_test.loc[:,ind], y_pred)
     print('The Accuracy of',ind,'is',acc)
     total_acc = total_acc + acc
     accuracy.append(acc)
+
+    cm = confusion_matrix(y_test.loc[:, ind], y_pred, labels = model.classes_)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+
+
+    disp.plot()
 
 
 print("The average accuracy is",total_acc/18)    
--- model_training/mutioutput.py
+++ model_training/mutioutput.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.multioutput import MultiOutputClassifier
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import accuracy_score
 
 from warnings import filterwarnings
@@ -17,9 +18,10 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, random
 
 log = LogisticRegression()
 
-clf = MultiOutputClassifier(log)
+clf = MultiOutputClassifier()
+
 clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
 print(y_pred.shape)
 #acc = accuracy_score(y_test, y_pred)
-# print('The accuracy is',clf.score(y_test, y_pred))
+print('The accuracy is',clf.score(x_test, y_test))
