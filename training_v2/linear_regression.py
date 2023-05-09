f89318f8efba9890b8a6fc00d6510806e0ef24a5
Abhishek Ballullaya K <abhishekbk1806@gmail.com>
2023-05-09 18:55:14 +0530

    nothing

--- training_v2/linear_regression.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import make_regression
-
-x, y = make_regression(n_samples=100, n_features=1, n_targets=1)
-
-print(x.shape)
-print(y.shape)
-
-x_train, x_test, y_train, y_test = train_test_split(x)
